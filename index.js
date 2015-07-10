321bb874acb09a919891959cbea9e6d7882f2dbc
Dan VerWeire <dverweire@gmail.com>
2015-07-10 14:43:03 -0400

    Handle arrays of columns to try as key/value fields

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 module.exports = function crossmash(ary, key, val) {
   var tmp = {}
 
-  ary.forEach(function (obj) {
-    tmp[obj[key]] = obj[val];
-  });
+  if (!Array.isArray(key) && !Array.isArray(val)) {
+    ary.forEach(function (obj) {
+      tmp[obj[key]] = obj[val];
+    });
+  }
+  else {
+    //make both key and val arrays
+    key = [].concat(key);
+    val = [].concat(val);
 
+    ary.forEach(function (obj) {
+      var k, v;
+
+      for (var x = 0; x < key.length; x++) {
+        k = key[x];
+
+        if (obj.hasOwnProperty(k)) {
+          break;
+        }
+      }
+
+      for (x = 0; x < key.length; x++) {
+        v = val[x];
+
+        if (obj.hasOwnProperty(v)) {
+          break;
+        }
+      }
+
+      tmp[obj[k]] = obj[v];
+    });
+  }
+  
   return tmp;
 }
--- test.js
+++ test.js
@@ -3,11 +3,11 @@ var cm = require('./')
     ;
 
 var ary = [
-    { name : 'Theon Greyjoy', title : 'Lord' }
+    { name : 'Theon Greyjoy', name2 : 'oops', title : 'Lord' }
     , { name : 'Daenerys Targaryen', title : 'Khaleesi' }
     , { name : 'Jon Snow', title : 'Bastard' }
     , { name : 'Tyrion Lannister', title : 'Master of Coin' }
-    , { name : 'Sansa Stark', title : 'Lady' }
+    , { name : 'Sansa Stark', title : 'Lady', title2 : 'Uh-oh' }
 ];
 
 var expect = {
@@ -20,3 +20,12 @@ var expect = {
 
 assert.deepEqual(cm(ary, 'name', 'title'), expect);
 
+var expect2 = {
+    'oops': 'Lord'
+    , 'Daenerys Targaryen': 'Khaleesi'
+    , 'Jon Snow': 'Bastard'
+    , 'Tyrion Lannister': 'Master of Coin'
+    , 'Sansa Stark': 'Uh-oh'
+}
+
+assert.deepEqual(cm(ary, ['name2', 'name'], ['title2', 'title']), expect2);
